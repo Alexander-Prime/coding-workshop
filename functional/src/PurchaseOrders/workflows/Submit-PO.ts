c8a0eea32745a3723f04e7369b16de76d633e02a
Alexander Peters <alex@synapsestudios.com>
2024-05-15 14:29:58 -0700

    Give names to Submit-PO chain fns

--- functional/src/PurchaseOrders/workflows/Submit-PO.ts
+++ functional/src/PurchaseOrders/workflows/Submit-PO.ts
@@ -1,28 +1,47 @@
 import {
   errAsync as ErrAsync,
   okAsync as OkAsync,
+  Result,
   ResultAsync,
 } from "neverthrow";
 
+import { DomainInvariantViolation } from "../../utilities/error/DomainInvariantViolation";
+import { Option } from "../../utilities/option";
 import { Uuid } from "../../utilities/uuid";
-import { PurchaseOrder } from "../domain/PurchaseOrder";
+import { DraftPurchaseOrder, PurchaseOrder } from "../domain/PurchaseOrder";
 import { PurchaseOrderRepo } from "../domain/PurchaseOrderRepo";
+
+const ensureExists =
+  (id: Uuid) =>
+  (maybePurchaseOrder: Option<PurchaseOrder>): Result<PurchaseOrder, Error> =>
+    maybePurchaseOrder.okOr(new Error(`Purchase order not found: ${id}`));
+
+const ensureIsDraft = (
+  maybeDraft: PurchaseOrder
+): ResultAsync<DraftPurchaseOrder, DomainInvariantViolation> =>
+  PurchaseOrder.isDraft(maybeDraft)
+    ? OkAsync(maybeDraft)
+    : ErrAsync(
+        new DomainInvariantViolation(
+          `Can't submit an order that's already been submitted: ${maybeDraft.id}`
+        )
+      );
+
+const pairNextPoNumber =
+  (PORepo: PurchaseOrderRepo) => (po: DraftPurchaseOrder) =>
+    ResultAsync.combine([OkAsync(po), PORepo.nextPoNumber(po.purchaser.org)]);
+
+const applyTo =
+  <F extends (...args: any[]) => unknown>(fn: F) =>
+  (args: Parameters<F>) =>
+    fn(...args) as ReturnType<F>;
 
 export const submitPO =
   ({ PORepo }: { PORepo: PurchaseOrderRepo }) =>
   (id: Uuid): ResultAsync<null, Error> =>
     PORepo.fetch(id)
-      .andThen((po) => po.okOr(new Error(`Purchase order not found: ${id}`)))
-      .andThen((po) =>
-        PurchaseOrder.isDraft(po)
-          ? OkAsync(po)
-          : ErrAsync(new Error(`Purchase order was already submitted: ${id}`))
-      )
-      .andThen((po) =>
-        ResultAsync.combine([
-          OkAsync(po),
-          PORepo.nextPoNumber(po.purchaser.org),
-        ])
-      )
-      .map(([po, poNumber]) => PurchaseOrder.submit(po, poNumber))
+      .andThen(ensureExists(id))
+      .andThen(ensureIsDraft)
+      .andThen(pairNextPoNumber(PORepo))
+      .map(applyTo(PurchaseOrder.submit))
       .andThen(PORepo.save);
