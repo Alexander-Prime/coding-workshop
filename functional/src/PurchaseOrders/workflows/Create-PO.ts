9d66a56fb1221bdee7ce902be0906f9d0a3dce3e
Alexander Peters <boringidiot@proton.me>
2024-07-09 13:31:16 -0700

    Refactor createPO w/ asyncEffect

--- functional/src/PurchaseOrders/workflows/Create-PO.ts
+++ functional/src/PurchaseOrders/workflows/Create-PO.ts
@@ -1,4 +1,5 @@
-import { ResultAsync } from "neverthrow";
+import { okAsync as OkAsync, ResultAsync } from "neverthrow";
+import { prop } from "remeda";
 
 import { NonEmptyArray } from "../../utilities/NonEmptyArray";
 import { Uuid } from "../../utilities/uuid";
@@ -7,12 +8,17 @@ import { PurchaseOrder } from "../domain/PurchaseOrder";
 import { PurchaseOrderRepo } from "../domain/PurchaseOrderRepo";
 import { Purchaser } from "../domain/Purchaser";
 
+const asyncEffect =
+  <T, E>(fn: (value: T) => ResultAsync<unknown, E>) =>
+  (value: T): ResultAsync<T, E> =>
+    fn(value).map(() => value);
+
 export const createPO =
   ({ PORepo }: { PORepo: PurchaseOrderRepo }) =>
   (
     purchaser: Purchaser,
     lineItems: NonEmptyArray<LineItem>
-  ): ResultAsync<Uuid, Error> => {
-    const po = PurchaseOrder.new(purchaser, lineItems);
-    return PORepo.save(po).map(() => po.id);
-  };
+  ): ResultAsync<Uuid, Error> =>
+    OkAsync(PurchaseOrder.new(purchaser, lineItems))
+      .andThen(asyncEffect(PORepo.save))
+      .map(prop("id"));
